ca526a507c94cdf9bc7dd364c118059f9e4f90cf
Karl Pokus <karl.pokus@gmail.com>
2019-01-05 16:23:32 +0100

    refactor ala mat ryer

--- service/pets/server.go
+++ service/pets/server.go
@@ -6,88 +6,81 @@ import (
 	"context"
 	"time"
 	"os"
+	router "github.com/julienschmidt/httprouter"
 	"github.com/mongodb/mongo-go-driver/mongo"
 	"github.com/karlpokus/opentracing-lab/service/utils/requestLogger"
 )
 
-var (
-	stdout *log.Logger = log.New(os.Stdout, "", 0)
-	stderr *log.Logger = log.New(os.Stderr, "", 0)
-)
-
-type Env struct {
+type server struct {
+	router *router.Router
+	stdout *log.Logger
+	stderr *log.Logger
 	pets *mongo.Collection
 }
 
-func (env *Env) findOnePetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-  	http.Error(w, http.StatusText(405), 405)
-    return
-  }
+func (s *server) applyRoutes() {
+	s.router.HandlerFunc("GET", "/pet/:name", s.findOnePetHandler)
+	s.router.HandlerFunc("GET", "/pets", s.findAllPetsHandler)
+	s.router.HandlerFunc("POST", "/pets/add", s.addOnePetHandler)
+}
 
-	petName := r.URL.Query().Get("name")
-	pet, err := findOnePet(env.pets, petName)
+func (s *server) findOnePetHandler(w http.ResponseWriter, r *http.Request) {
+	petName := router.ParamsFromContext(r.Context())[0].Value
+	pet, err := findOnePet(s.pets, petName)
 	if err != nil {
-		stderr.Print(err.Error())
+		s.stderr.Print(err.Error())
 		http.Error(w, http.StatusText(404), 404)
 		return
 	}
 
-	stdout.Println("ok")
+	s.stdout.Println("ok")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(pet)
 }
 
-func (env *Env) findAllPetsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-  	http.Error(w, http.StatusText(405), 405)
-    return
-  }
-
-	pets, err := findAllPets(env.pets)
+func (s *server) findAllPetsHandler(w http.ResponseWriter, r *http.Request) {
+	pets, err := findAllPets(s.pets)
 	if err != nil {
-		stderr.Print(err.Error())
+		s.stderr.Print(err.Error())
 		http.Error(w, http.StatusText(400), 400)
 		return
 	}
 
-	stdout.Println("ok")
+	s.stdout.Println("ok")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(pets)
 }
 
-func (env *Env) addOnePetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-  	http.Error(w, http.StatusText(405), 405)
-    return
-  }
+func (s *server) addOnePetHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	if err := addOnePet(env.pets, r.Body); err != nil {
-		stderr.Print(err.Error())
+	if err := addOnePet(s.pets, r.Body); err != nil {
+		s.stderr.Print(err.Error())
 		http.Error(w, http.StatusText(400), 400)
 		return
 	}
 
-	stdout.Println("ok")
+	s.stdout.Println("ok")
 	w.Write([]byte("ok"))
 }
 
 func main() {
+	s := &server{
+		router: router.New(),
+		stdout: log.New(os.Stdout, "", 0),
+		stderr: log.New(os.Stderr, "", 0),
+	}
+	s.applyRoutes()
+
 	ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
 	mongoConnString := "mongodb://localhost:27017"
 	client, err := mongo.Connect(ctx, mongoConnString)
 	if err != nil {
-		stderr.Fatal(err.Error())
+		s.stderr.Fatal(err.Error())
 	}
-	stdout.Printf("connected to %s", mongoConnString)
 
-	env := &Env{pets: client.Database("pets").Collection("pets")}
-
-	http.HandleFunc("/pet", env.findOnePetHandler);
-	http.HandleFunc("/pets", env.findAllPetsHandler);
-	http.HandleFunc("/pet/add", env.addOnePetHandler)
-
-	stdout.Print("listening on port 9113")
-	stderr.Fatal(http.ListenAndServe(":9113", requestLogger.Log(stdout, http.DefaultServeMux)))
+	s.pets = client.Database("pets").Collection("pets")
+	s.stdout.Printf("connected to %s", mongoConnString)
+	s.stdout.Print("listening on port 9113")
+	s.stderr.Fatal(http.ListenAndServe(":9113", requestLogger.Log(s.stdout, s.router)))
 }
